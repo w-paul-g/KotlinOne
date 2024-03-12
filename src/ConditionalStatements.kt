63bbf07affbad91a6a95f5a2c4355cfa213c23c5
w-paul-g <w.paul.gichuki@gmail.com>
2024-03-12 13:43:14 +0300

    initial commit

--- src/ConditionalStatements.kt
+++ src/ConditionalStatements.kt
@@ -1,6 +1,8 @@
 fun main(args: Array<String>) {
     //Conditional Statements
 
+    //If Statement
+
     //e.g.
     var num = -100
     if (num < 0){
@@ -10,7 +12,7 @@ fun main(args: Array<String>) {
         println("This is a positive number.")
     }
 
-    //
+    //Multiple If Conditions
 
     //e.g
     var age = 19
@@ -30,7 +32,42 @@ fun main(args: Array<String>) {
         println("You're an adult likely to be out of school.")
     }
 
+    //When Statement
 
+    //e.g.
+    var letter = 'p'
+    when(letter){
+        'a'-> println("a is a vowel")
+        'e'-> println("e is a vowel")
+        'i'-> println("i is a vowel")
+        'o'-> println("o is a vowel")
+        'u'-> println("u is a vowel")
+        else-> println("$letter is a consonant.")
+    }
 
+    var number = 3
+    when(number){
+        1 -> println("Too bad! You failed the game!")
+        2 -> println("Wow! You almost got the number!")
+        3 -> println("Congratulation! You are the WINNER!")
+        else -> println("$number is invalid! Try Again!")
+    }
+
+    //e.g BMI Calculator
+    var height = 1.32
+    var weight = 70
+    var bodymassindex = (weight/(height*height))
+    if (bodymassindex < 18){
+        println("You're underweight")
+    }
+    else if (18 <= bodymassindex){
+        println("You're normal")
+    }
+    else if (25 <= bodymassindex){
+        println("You're overweight")
+    }
+    else if(30 < bodymassindex){
+        println("You're obese")
+    }
 
 }
